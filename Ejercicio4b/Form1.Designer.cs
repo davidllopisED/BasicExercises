01bec8044c548bbcf9f508b21b775ff36870ba69
davidllopisED <davidllopis@paucasesnovescifp.cat>
2020-10-16 11:12:33 +0200

    Exercise 4b is very similar to 4a but with the difference that when we select the operation to perform, we do it through a listBox

---
+++ Ejercicio4b/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace Ejercicio4b
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Operand1 = new System.Windows.Forms.TextBox();
+            this.Operand2 = new System.Windows.Forms.TextBox();
+            this.Calculate = new System.Windows.Forms.Button();
+            this.listBox = new System.Windows.Forms.ListBox();
+            this.Operation = new System.Windows.Forms.TextBox();
+            this.Result = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Reset = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Operand1
+            // 
+            this.Operand1.Location = new System.Drawing.Point(74, 102);
+            this.Operand1.Name = "Operand1";
+            this.Operand1.Size = new System.Drawing.Size(100, 22);
+            this.Operand1.TabIndex = 0;
+            this.Operand1.TextChanged += new System.EventHandler(this.Operation1_TextChanged);
+            // 
+            // Operand2
+            // 
+            this.Operand2.Location = new System.Drawing.Point(74, 166);
+            this.Operand2.Name = "Operand2";
+            this.Operand2.Size = new System.Drawing.Size(100, 22);
+            this.Operand2.TabIndex = 1;
+            // 
+            // Calculate
+            // 
+            this.Calculate.Location = new System.Drawing.Point(183, 211);
+            this.Calculate.Name = "Calculate";
+            this.Calculate.Size = new System.Drawing.Size(75, 23);
+            this.Calculate.TabIndex = 2;
+            this.Calculate.Text = "Calculate";
+            this.Calculate.UseVisualStyleBackColor = true;
+            this.Calculate.Click += new System.EventHandler(this.Calculate_Click);
+            // 
+            // listBox
+            // 
+            this.listBox.FormattingEnabled = true;
+            this.listBox.ItemHeight = 16;
+            this.listBox.Items.AddRange(new object[] {
+            "-",
+            "+"});
+            this.listBox.Location = new System.Drawing.Point(335, 84);
+            this.listBox.Name = "listBox";
+            this.listBox.Size = new System.Drawing.Size(87, 52);
+            this.listBox.TabIndex = 3;
+            this.listBox.SelectedIndexChanged += new System.EventHandler(this.listBox_SelectedIndexChanged);
+            // 
+            // Operation
+            // 
+            this.Operation.Location = new System.Drawing.Point(335, 199);
+            this.Operation.Name = "Operation";
+            this.Operation.Size = new System.Drawing.Size(100, 22);
+            this.Operation.TabIndex = 4;
+            // 
+            // Result
+            // 
+            this.Result.Location = new System.Drawing.Point(554, 137);
+            this.Result.Name = "Result";
+            this.Result.Size = new System.Drawing.Size(100, 22);
+            this.Result.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(312, 179);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(163, 17);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Operación Seleccionada";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(266, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(222, 17);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Seleccione la operación a realizar";
+            // 
+            // Reset
+            // 
+            this.Reset.Location = new System.Drawing.Point(570, 198);
+            this.Reset.Name = "Reset";
+            this.Reset.Size = new System.Drawing.Size(75, 23);
+            this.Reset.TabIndex = 9;
+            this.Reset.Text = "Reset";
+            this.Reset.UseVisualStyleBackColor = true;
+            this.Reset.Click += new System.EventHandler(this.Reset_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Reset);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Result);
+            this.Controls.Add(this.Operation);
+            this.Controls.Add(this.listBox);
+            this.Controls.Add(this.Calculate);
+            this.Controls.Add(this.Operand2);
+            this.Controls.Add(this.Operand1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Operand1;
+        private System.Windows.Forms.TextBox Operand2;
+        private System.Windows.Forms.Button Calculate;
+        private System.Windows.Forms.ListBox listBox;
+        private System.Windows.Forms.TextBox Operation;
+        private System.Windows.Forms.TextBox Result;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button Reset;
+    }
+}
+
---
+++ Ejercicio4b/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio4b
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void listBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+    
+        }
+
+        private void Calculate_Click(object sender, EventArgs e)
+        {
+            if (listBox.Text=="+")
+            {
+                int operand1sum, operand2sum, sum;
+                operand1sum = Convert.ToInt32(Operand1.Text);
+                operand2sum = Convert.ToInt32(Operand2.Text);
+                sum = operand1sum + operand2sum;
+                Result.Text = sum.ToString();
+                Operation.Text = "+";
+            }
+            if (listBox.Text == "-")
+            {
+                int operand1sub, operand2sub, sub;
+                operand1sub = Convert.ToInt32(Operand1.Text);
+                operand2sub = Convert.ToInt32(Operand2.Text);
+                sub = operand1sub - operand2sub;
+                Result.Text = sub.ToString();
+                Operation.Text = "-";
+            }
+            if (listBox.Text != "+" && listBox.Text != "-")
+            {
+                MessageBox.Show("Please try Sum or Substract");
+            }
+        }
+
+        private void Operation1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void Sum_Click(object sender, EventArgs e)
+        {
+            listBox.Text = "+";
+        }
+
+        private void Substract_Click(object sender, EventArgs e)
+        {
+            listBox.Text = "-";
+        }
+
+        private void Reset_Click(object sender, EventArgs e)
+        {
+            Operand1.Clear();
+            Operand2.Clear();
+            Operation.Clear();
+            Result.Clear();
+        }
+    }
+}
