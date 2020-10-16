497e3a4b8cd84f083f40a648843a3f54c0beb023
davidllopisED <davidllopis@paucasesnovescifp.cat>
2020-10-16 19:24:54 +0200

    This exercise is based on creating two buttons (even and odd) and for example if we press "even" we will get the even numbers from one to ten and if we press the "odd" button it will show us the odd numbers from one to ten

---
+++ Ejercicio9/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace Ejercicio9
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ComboBox = new System.Windows.Forms.ComboBox();
+            this.Even = new System.Windows.Forms.Button();
+            this.Odd = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // ComboBox
+            // 
+            this.ComboBox.FormattingEnabled = true;
+            this.ComboBox.Location = new System.Drawing.Point(51, 64);
+            this.ComboBox.Name = "ComboBox";
+            this.ComboBox.Size = new System.Drawing.Size(121, 24);
+            this.ComboBox.TabIndex = 0;
+            this.ComboBox.SelectedIndexChanged += new System.EventHandler(this.ComboBox_SelectedIndexChanged);
+            // 
+            // Even
+            // 
+            this.Even.Location = new System.Drawing.Point(263, 65);
+            this.Even.Name = "Even";
+            this.Even.Size = new System.Drawing.Size(75, 23);
+            this.Even.TabIndex = 1;
+            this.Even.Text = "Even";
+            this.Even.UseVisualStyleBackColor = true;
+            this.Even.Click += new System.EventHandler(this.Even_Click);
+            // 
+            // Odd
+            // 
+            this.Odd.Location = new System.Drawing.Point(368, 65);
+            this.Odd.Name = "Odd";
+            this.Odd.Size = new System.Drawing.Size(75, 23);
+            this.Odd.TabIndex = 2;
+            this.Odd.Text = "Odd";
+            this.Odd.UseVisualStyleBackColor = true;
+            this.Odd.Click += new System.EventHandler(this.Odd_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(51, 223);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(391, 22);
+            this.textBox1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Odd);
+            this.Controls.Add(this.Even);
+            this.Controls.Add(this.ComboBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox ComboBox;
+        private System.Windows.Forms.Button Even;
+        private System.Windows.Forms.Button Odd;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Ejercicio9/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio9
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Even_Click(object sender, EventArgs e)
+        {
+            ComboBox.Items.Clear();
+            for (int i =1;i<=10;i++)
+            {
+                if (i % 2 == 0)
+                {
+                    ComboBox.Items.Add(i.ToString());
+                }
+            }
+        }
+
+        private void Odd_Click(object sender, EventArgs e)
+        {
+            ComboBox.Items.Clear();
+            for (int i = 1; i <= 10; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    ComboBox.Items.Add(i.ToString());
+                }
+            }
+        }
+
+        private void ComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            textBox1.Text = ComboBox.Text;
+        }
+    }
+}
